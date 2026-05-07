c187247ec82728578adeba73361eea6d60b8790e
Farzad Sanaie <farzad@sanaie.se>
2026-05-07 13:28:37 +0200

    Initated project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
-    </div>
+    <>
+      <div className="flex flex-col flex-1 items-center justify-center bg-white text-black font-sans">
+        <main>
+          <h1>The Zoo</h1>
+          <p>Välkommna till vårt zoo</p>
+          <Link href={"/animals"}>Se vår djur</Link>
+        </main>
+      </div>
+    </>
   );
 }
