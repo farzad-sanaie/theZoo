5c2297872c33164c780e2bde55e5bd2c505e76c3
Farzad Sanaie <farzad@sanaie.se>
2026-05-07 21:24:48 +0200

    Added LocalStorage

--- app/services/animalService.ts
+++ app/services/animalService.ts
@@ -2,9 +2,20 @@ import { Animal } from "../models/Animal";
 import { get } from "./serviceBase";
 
 const BASE_URL = "https://animals.azurewebsites.net/api/animals";
+const ANIMALS_LS_KEY = "animals";
 
 export const getAnimals = async () => {
-  const animals = await get<Animal[]>(BASE_URL);
+  const animalsFromLocalStorage = localStorage.getItem(ANIMALS_LS_KEY);
 
+  if (animalsFromLocalStorage) {
+    return JSON.parse(animalsFromLocalStorage) as Animal[];
+  }
+
+  const animals = await get<Animal[]>(BASE_URL);
+  localStorage.setItem(ANIMALS_LS_KEY, JSON.stringify(animals));
   return animals;
+};
+
+export const saveAnimalsToLocalStorage = (animals: Animal[]) => {
+  localStorage.setItem(ANIMALS_LS_KEY, JSON.stringify(animals));
 };
